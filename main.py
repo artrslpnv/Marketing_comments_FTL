458d34aeb1fed08a472070971d79bece8b3c7344
arslepnev2000 <ArTuRsl2000>
2019-10-29 04:01:15 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dialog_bot_sdk.bot import DialogBot
-import grpc
+import grpcio
 from globals import app as application
 import sys
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     application.run(port=5000 if len(sys.argv) == 1 else int(sys.argv[1]), threaded=True)
     bot = DialogBot.get_secure_bot(
         'hackathon-mob.transmit.im',  # bot endpoint (specify different endpoint if you want to connect to your on-premise environment)
-        grpc.ssl_channel_credentials(), # SSL credentials (empty by default!)
+        grpcio.ssl_channel_credentials(), # SSL credentials (empty by default!)
         'd01418175003f742d2f2718cd66a177693ef4321',  # bot token
         verbose=False # optional parameter, when it's True bot prints info about the called methods, False by default
     )
