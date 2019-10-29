e76163df9511f67406de5c06f9a31bef1d141e3c
arslepnev2000 <ArTuRsl2000>
2019-10-29 04:23:05 +0300

    Initial commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dialog_bot_sdk.bot import DialogBot
 import grpcio
 from globals import app as application
 import sys
+from log import logger
 
 def on_msg(*params):
     print('on msg', params)
@@ -12,10 +13,11 @@ def on_msg(*params):
 
 if __name__ == '__main__':
     application.run(port=5000 if len(sys.argv) == 1 else int(sys.argv[1]), threaded=True)
+    print(grpcio.ssl_channel_credentials())
     bot = DialogBot.get_secure_bot(
         'hackathon-mob.transmit.im',  # bot endpoint (specify different endpoint if you want to connect to your on-premise environment)
         grpcio.ssl_channel_credentials(), # SSL credentials (empty by default!)
-        'd01418175003f742d2f2718cd66a177693ef4321',  # bot token
+        '592e37a534ced85ee9f06561dce1b3e1985f94f2',  # bot token
         verbose=False # optional parameter, when it's True bot prints info about the called methods, False by default
     )
     bot.messaging.on_message(on_msg)
